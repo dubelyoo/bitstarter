3e14315639bc029a98d36dc528c1e29a33f8759b
dubleyoo <user@6m.(none)>
2013-07-09 23:44:23 -0700

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
     myBuffer = new Buffer(data);
   }
 
-  response.send(myBuffer.toString();
+  response.send(myBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
